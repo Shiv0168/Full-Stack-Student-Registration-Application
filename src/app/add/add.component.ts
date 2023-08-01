218741822f282554d42d9c80570969ab5988e7dc
SHIV KUAMR SINGH <shiv01.chemengg@gmail.com>
2023-08-01 19:36:19 +0530

    remove unwanted comment

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiServiceService } from '../api-service.service';
 import { Router } from '@angular/router';
-import { Student } from '../Model/Student';
 import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
